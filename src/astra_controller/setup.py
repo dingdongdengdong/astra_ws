b7e3d45cf6f0847567dbe6320d32216591ddac33
Imggaggu <ggaggu@snu.ac.kr>
2025-05-03 20:48:05 +0900

    [Feat] hello publisher&subscriber setup.py

--- src/astra_controller/setup.py
+++ src/astra_controller/setup.py
@@ -40,6 +40,8 @@ setup(
             'base_node = astra_controller.base_node:main',
             'head_node = astra_controller.head_node:main',
             'cam_node = astra_controller.cam_node:main',
+            'hello_publisher = astra_controller.hello_publisher:main',
+            'hello_node = astra_controller.hello_node:main',
         ],
     },
 )
